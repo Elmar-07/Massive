15dd97e9b425a5246fdf70f51479a37e000f5476
Elmar-07 <nurmamedov572@gmail.com>
2022-12-19 19:40:18 +0400

    Massive

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+
+
+let words = [
+    [ 'banana', 'apple', 'orange'],
+    [ 'lemon', 'cucumber', 'tomato']
+]
+
+document.write("My name is: " + words[0][1][4] + words[1][0][0]+ words[1][1][4]+ words[0][0][1]+ words[0][2][1])
